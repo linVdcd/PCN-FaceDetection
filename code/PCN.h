4b252563b25cedfb5a420264fcc5a31c99bacd5a
linVdcd <lma5547@sina.com>
2018-11-19 10:02:04 +0800

    add cmakelists

--- code/PCN.h
+++ code/PCN.h
@@ -24,64 +24,14 @@ struct Window
     {}
 };
 
-cv::Point RotatePoint(int x, int y, float centerX, float centerY, float angle)
-{
-    x -= centerX;
-    y -= centerY;
-    float theta = -angle * M_PI / 180;
-    int rx = int(centerX + x * std::cos(theta) - y * std::sin(theta));
-    int ry = int(centerY + x * std::sin(theta) + y * std::cos(theta));
-    return cv::Point(rx, ry);
-}
+cv::Point RotatePoint(int x, int y, float centerX, float centerY, float angle);
+void DrawLine(cv::Mat img, std::vector<cv::Point> pointList);
 
-void DrawLine(cv::Mat img, std::vector<cv::Point> pointList)
-{
-    int thick = 2;
-    CvScalar cyan = CV_RGB(0, 255, 255);
-    CvScalar blue = CV_RGB(0, 0, 255);
-    cv::line(img, pointList[0], pointList[1], cyan, thick);
-    cv::line(img, pointList[1], pointList[2], cyan, thick);
-    cv::line(img, pointList[2], pointList[3], cyan, thick);
-    cv::line(img, pointList[3], pointList[0], blue, thick);
-}
+void DrawFace(cv::Mat img, Window face);
 
-void DrawFace(cv::Mat img, Window face)
-{
-    int x1 = face.x;
-    int y1 = face.y;
-    int x2 = face.width + face.x - 1;
-    int y2 = face.width + face.y - 1;
-    int centerX = (x1 + x2) / 2;
-    int centerY = (y1 + y2) / 2;
-    std::vector<cv::Point> pointList;
-    pointList.push_back(RotatePoint(x1, y1, centerX, centerY, face.angle));
-    pointList.push_back(RotatePoint(x1, y2, centerX, centerY, face.angle));
-    pointList.push_back(RotatePoint(x2, y2, centerX, centerY, face.angle));
-    pointList.push_back(RotatePoint(x2, y1, centerX, centerY, face.angle));
-    DrawLine(img, pointList);
-}
 
-cv::Mat CropFace(cv::Mat img, Window face, int cropSize)
-{
-    int x1 = face.x;
-    int y1 = face.y;
-    int x2 = face.width + face.x - 1;
-    int y2 = face.width + face.y - 1;
-    int centerX = (x1 + x2) / 2;
-    int centerY = (y1 + y2) / 2;
-    cv::Point2f srcTriangle[3];
-    cv::Point2f dstTriangle[3];
-    srcTriangle[0] = RotatePoint(x1, y1, centerX, centerY, face.angle);
-    srcTriangle[1] = RotatePoint(x1, y2, centerX, centerY, face.angle);
-    srcTriangle[2] = RotatePoint(x2, y2, centerX, centerY, face.angle);
-    dstTriangle[0] = cv::Point(0, 0);
-    dstTriangle[1] = cv::Point(0, cropSize - 1);
-    dstTriangle[2] = cv::Point(cropSize - 1, cropSize - 1);
-    cv::Mat rotMat = cv::getAffineTransform(srcTriangle, dstTriangle);
-    cv::Mat ret;
-    cv::warpAffine(img, ret, rotMat, cv::Size(cropSize, cropSize));
-    return ret;
-}
+cv::Mat CropFace(cv::Mat img, Window face, int cropSize);
+
 
 class PCN
 {
